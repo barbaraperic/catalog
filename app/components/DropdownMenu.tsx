677d166e3cb4365c4f9ade27a7389b098d4ff479
Barbara Peric <b.peric10@gmail.com>
2024-03-18 15:05:05 +0100

    dropdown component

--- app/components/DropdownMenu.tsx
+++ app/components/DropdownMenu.tsx
@@ -4,9 +4,19 @@ import styles from './DropdownMenu.module.scss'
 
 export default function DropdownMenu() {
     return (
-        <nav>
+        <nav className={styles.nav}>
             <div className={styles.icon}></div>
-            <div className={styles.dropdown}></div>
+            <div className={styles.dropdown}>
+                <ul>
+                    <li>Notes</li>
+                    <li>Projects</li>
+                </ul>
+                <ul>
+                    <li>About</li>
+                    <li>LinkedIn</li>
+                    <li>GitHub</li>
+                </ul>
+            </div>
         </nav>
     )
 }
