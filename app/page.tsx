df49e4a08a763f3659b597bb870060d7939a3f5b
Barbara Peric <b.peric10@gmail.com>
2024-03-18 13:47:13 +0100

    pagre layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,21 @@ import styles from './page.module.scss'
 
 export default function Home() {
     return (
-        <main>
-            <header className={styles.wrapper}>
-                <div className={`${styles.box} ${styles.header}`}>Header</div>
-                <div className={`${styles.box} ${styles.sidebar}`}>Sidebar</div>
-                <div className={`${styles.box} ${styles.content}`}>
-                    Content
-                    <br /> More content than we had before so this column is now
-                    quite tall.
-                </div>
-                <div className={`${styles.box} ${styles.footer}`}>Footer</div>
-            </header>
-        </main>
+        <div className={styles.container}>
+            <nav className={styles.nav}>
+                <ul>
+                    <li>About</li>
+                    <li>LinkedIn</li>
+                    <li>GitHub</li>
+                </ul>
+                <ul>
+                    <li>Notes</li>
+                    <li>Projects</li>
+                </ul>
+            </nav>
+            <header className={styles.header}></header>
+            <main></main>
+            <footer></footer>
+        </div>
     )
 }
